ef955ea0f9788ddbb568e722875a82165ced9b13
Ashifa7 <84831054+Ashifa7@users.noreply.github.com>
2021-06-11 09:48:20 +0530

    Create Arithmetic Operators.py

---
+++ Task4 functions and operators/Arithmetic Operators.py
@@ -0,0 +1,24 @@
+# Arithmetic Operators:
+
+num1 = 20
+num2 = 10
+
+print("Arithmetic Operators:")
+
+result = num1 + num2
+print("Addition is: ", result)
+
+result = num1 - num2
+print("Substraction is: ", result)
+
+result = num1 * num2
+print("Multiplication is: ", result)
+
+result = num1 / num2
+print("Division is: ", result)
+
+result = num1 // num2
+print("Integer Division is: ", result)
+
+result = num1 % num2
+print("Modulo is: ", result)
