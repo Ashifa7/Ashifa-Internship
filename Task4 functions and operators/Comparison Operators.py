d4c4b9564321db041e416f755f4a682c7f899351
Ashifa7 <84831054+Ashifa7@users.noreply.github.com>
2021-06-11 09:50:39 +0530

    Create Comparison Operators.py

---
+++ Task4 functions and operators/Comparison Operators.py
@@ -0,0 +1,24 @@
+# Comparison Operators:
+num1 = 25
+num2 = 17
+print("\n")
+print("Comparison Operators:")
+
+result = num1 > num2
+print("Num1 is Greater than Num 2: ", result)
+
+result = num1 < num2
+print("Num1 is Less than Num 2: ", result)
+
+result = num1 == num2
+print("Num1 is Equal to Num 2: ", result)
+
+result = num1 >= num2
+print("Num1 is Greater than or equal to Num 2: ", result)
+
+result = num1 <= num2
+print("Num1 is Less than or equal to Num 2: ", result)
+
+result = num1 != num2
+print("Num1 is not euqal to Num 2: ", result)
+print("\n")
