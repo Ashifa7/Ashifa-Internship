ac9f09e3857e7130a6753b4c6edb494438a441f7
Ashifa7 <84831054+Ashifa7@users.noreply.github.com>
2021-06-11 09:57:54 +0530

    Create membership operators.py

---
+++ Task4 functions and operators/membership operators.py
@@ -0,0 +1,11 @@
+# Membership Operators:
+num1 = 14
+num2 = 89
+lst = [45, 67, 12, 89, 41, 14, "Hello"]
+
+
+print("Membership Operators:")
+
+print("num1 in lst:",num1 in lst)
+print("num2 in lst:",num2 in lst)
+print("num1 not in lst:",num1 not in lst)
